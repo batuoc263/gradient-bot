d6d72fb519d973cba224a07aaa765a8ae7c96b61
Chau Nguyen <nguyendinhchau705@gmail.com>
2024-11-10 17:20:48 +0700

    Update no proxy

--- start_without_proxy.js
+++ start_without_proxy.js
@@ -1,7 +1,6 @@
 // 1. read proxies from file
 const fs = require('fs')
 const path = require('path')
-const proxies = fs.readFileSync(path.resolve(__dirname, 'proxies.txt'), 'utf-8').split('\n').filter(Boolean)
 
 // 2. start pm2 with PROXY env
 const { execSync } = require('child_process')
